686604de32fff09733382a78133cbd99fd3da2a0
Diego Malvasi (Macro) <diego.malvasi@hotmail.com>
2022-03-14 14:12:00 -0300

    FINAL - TERRAFORM - ANSIBLE - AZURE | CREDENCIALES

--- azure/terraform/credenciales.tf
+++ azure/terraform/credenciales.tf
@@ -1,24 +1,24 @@
 variable "subscription_id" {
   type = string
   description = "Id de subcripcion"
-  default = "XX"
+  #default = "XX"
 }
 
 variable "client_id" {
   type = string
   description = "Id de cliente"
-  default = "XX"
+  #default = "XX"
 }
 
 variable "client_secret" {
   type = string
   description = "Id clave secreta"
-  default = "XX"
+  #default = "XX"
 }
 
 variable "tenant_id" {
   type = string
   description = "Id tenant"
-  default = "XX"
+  #default = "XX"
 }
 
